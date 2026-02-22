b6590d2aaac45c06fd355330858c76bc5d463994
xzxhub0-source <xzxhub0@gmail.com>
2026-02-21 19:29:36 -0500

    Update postcss.config.js
    Signed-off-by: xzxhub0-source <xzxhub0@gmail.com>

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,6 +1,18 @@
+// web/postcss.config.js
 module.exports = {
   plugins: {
+    // Tailwind CSS plugin
     tailwindcss: {},
+    
+    // Autoprefixer for adding vendor prefixes
     autoprefixer: {},
+    
+    // Add PostCSS Preset Env for modern CSS features (optional but recommended)
+    // 'postcss-preset-env': {
+    //   stage: 3,
+    //   features: {
+    //     'nesting-rules': true,
+    //   },
+    // },
   },
 };
