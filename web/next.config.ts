1eada9afe9bb74e02d5019fd7251f58c85e2d5e2
xzxhub0-source <xzxhub0@gmail.com>
2026-02-22 07:39:29 -0500

    Update next.config.ts
    Signed-off-by: xzxhub0-source <xzxhub0@gmail.com>

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,11 +1,8 @@
-// web/next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Remove the invalid 'server' key
   images: {
     unoptimized: true,
   },
-  // That's it! No server configuration needed
 };
 
 module.exports = nextConfig;
