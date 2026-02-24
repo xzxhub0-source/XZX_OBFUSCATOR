3244dfde9d8783647f2b7670d9b03df6f8e42bdd
xzxhub0-source <xzxhub0@gmail.com>
2026-02-24 06:39:30 -0500

    Update tailwind.config.ts
    Signed-off-by: xzxhub0-source <xzxhub0@gmail.com>

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,20 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
@@ -58,5 +51,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("tailwindcss-animate")],
+};
+
+export default config;
